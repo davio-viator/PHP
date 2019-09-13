305054650cbb35f9a2a9ee0035ab3599d9509656
viatord <socialempire971@hotmail.fr>
2019-09-13 14:55:23 +0200

    Voiture

---
+++ creerVoiture.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <meta charset="utf-8" />
+        <title> Voiture </title>
+    </head>
+   
+    <body>
+      <?php
+      require_once("Voiture.php");
+      $marque=$_POST["marque"];
+      $couleur=$_POST["couleur"];
+      $immatriculation=$_POST["immatriculation"];
+      $Voiture = new Voiture($marque,$couleur,$immatriculation);
+      $Voiture->afficher();
+      ?>
+    </body>
+</html>
