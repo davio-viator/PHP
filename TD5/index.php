7934cf6f5420a00d7ac4c7248bd18ac54034b283
davio-viator <davio.viator@etu.umontpellier.fr>
2019-10-17 21:58:44 +0200

    maj td5

--- TD5/index.php
+++ TD5/index.php
@@ -2,12 +2,15 @@
 <!DOCTYPE html>
     <html>
         <head>
-            <title></title>
+            <meta charset="UTF-8">
+            <title>Liste des trajets</title>
         </head>
         <body>
             <?php
             require("controller/routeur.php");
+            require ("view/view.php");
             ?>
+        <div></div>
 
         </body>
     </html>
--- TD5/view/view.php
+++ TD5/view/view.php
@@ -5,11 +5,29 @@
             <title><?php echo $pagetitle; ?></title>
         </head>
         <body>
+        <div>
+            <nav>
+                <ul>
+                    <li> <a href = "index.php?action=readAll">liste des voiture</a></li>
+                    <li><a href = "index.php?action=readAll&controller=utilisateur">Liste des utilisateur</a></li>
+                    <li><a href = "index.php?action=readAll&controller=trajet">Liste des trajet</a></li>
+                </ul>
+            </nav>
+        </div>
             <?php
+            require_once ('lib/File.php');
             // Si $controleur='voiture' et $view='list',
             // alors $filepath="/chemin_du_site/view/voiture/list.php"
+            //echo array("view", $controller, "$view.php");
             $filepath = File::build_path(array("view", $controller, "$view.php"));
-            require $filepath;
+            var_dump($filepath);
+            require ($filepath);
             ?>
+
+        <footer>
+            <p style="border: 1px solid black;text-align:right;padding-right:1em;">
+                Site de covoiturage de ...
+            </p>
+        </footer>
         </body>
     </html>
